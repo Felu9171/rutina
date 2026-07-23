3ead7a79dbf15f1327627d006172380de6965b2d
Felu9171 <felipepieretti2006@gmail.com>
2026-07-23 09:35:12 -0300

    Renombrar Intensivos, reparto de carga a finde/viernes (7h sueno) y madrugon manual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rutina-v5';
+const CACHE='rutina-v6';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])).catch(()=>{}));
   self.skipWaiting();
