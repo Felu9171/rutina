00ac061b831282241cf801e3687ca33af3d49583
Felu9171 <felipepieretti2006@gmail.com>
2026-07-23 09:12:09 -0300

    Rediseno vista Hoy tipo calendario con emojis + boton adelantar siempre visible

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='rutina-v3';
+const CACHE='rutina-v4';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])).catch(()=>{}));
   self.skipWaiting();
